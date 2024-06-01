a3c1f4c2f3fb7c7d309c5ff38dbc636b00007e3f
DariusChit <darius.chitoroaga@proton.me>
2024-06-01 15:33:54 +0100

    virt

--- nixos/virt.nix
+++ nixos/virt.nix
@@ -1,6 +1,16 @@
-{...}: {
+{pkgs, ...}: {
   virtualisation = {
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [pkgs.OVMFFull.fd];
+      };
+    };
+    spiceUSBRedirection.enable = true;
     podman.enable = true;
   };
+
+  programs.virt-manager.enable = true;
 }
