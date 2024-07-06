dc285cb40de40e9d736e594a22440a09c9dbf877
DariusChit <darius.chitoroaga@proton.me>
2024-07-06 18:57:38 +0100

    updated git settings

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -4,5 +4,11 @@
     userName = "DariusChit";
     userEmail = "darius.chitoroaga@proton.me";
     lfs.enable = true;
+    extraConfig = {
+      color.ui = true;
+      core.editor = "nvim";
+      credential.helper = "store";
+      push.autoSetupRemote = true;
+    };
   };
 }
