a0cfd21d6ec534d0a9e7a3bd7588e7cfab822610
DariusChit <darius.chitoroaga@proton.me>
2024-08-03 15:45:23 +0100

    added user command for disabling auto formatting nvim

--- home/cli/nvim/lua/plugins/lsp/conform.lua
+++ home/cli/nvim/lua/plugins/lsp/conform.lua
@@ -1,6 +1,26 @@
+-- Create command to temporarily disable formatting
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
+
 return {
 	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 	opts = {},
 	config = function()
 		require("conform").setup({
@@ -20,11 +40,14 @@ return {
 				yaml = { { "prettierd", "prettier" } },
 				nix = { "alejandra" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				-- async = true,
-				timeout = 1000,
-			},
+
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_format = "fallback" }
+			end,
 		})
 	end,
 }
