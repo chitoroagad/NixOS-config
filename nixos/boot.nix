9ee00c6c25d6f5abf287180f651e8010601e4301
DariusChit <darius.chitoroaga@proton.me>
2024-08-03 16:38:42 +0100

    turned plymouth and quiet boot back on

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -19,21 +19,20 @@
     # LUKS
     initrd.luks.devices."luks-1ff10d2e-5580-4c23-9efe-60fc2771934b".device = "/dev/disk/by-uuid/1ff10d2e-5580-4c23-9efe-60fc2771934b";
 
-    # Newer kernel
     kernelPackages = pkgs.linuxPackages_6_9;
-    # kernelParams = [
-    #   "quiet"
-    # ];
-    #
-    # # Plymouth
-    # plymouth = {
-    #   enable = true;
-    #   # Catppuccin theme enabled in desktop.nix
-    # };
-    #
-    # # Quiet boot
-    # consoleLogLevel = 0;
-    # initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+    ];
+
+    # Plymouth
+    plymouth = {
+      enable = true;
+      # Catppuccin theme enabled in desktop.nix
+    };
+
+    # Quiet boot
+    consoleLogLevel = 0;
+    initrd.verbose = false;
 
     # Use systemd for initramfs rather than udev
     initrd.systemd.enable = true;
