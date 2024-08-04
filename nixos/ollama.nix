e0c9ff3db91cdf2df4c8c2d3eb9aa32da3601d6d
DariusChit <darius.chitoroaga@pm.me>
2024-08-04 22:58:21 +0100

    fixed gpu support for ollama

--- nixos/ollama.nix
+++ nixos/ollama.nix
@@ -1,10 +1,12 @@
 {pkgs, ...}: {
   services.ollama = {
-    package = pkgs.stable.ollama;
+    package = pkgs.ollama;
     enable = true;
     acceleration = "rocm";
+    rocmOverrideGfx = "11.0.1";
     environmentVariables = {
       OLLAMA_KEEP_ALIVE = "30s";
+      OLLAMA_LLM_LIBRARY = "rocm";
     };
   };
 }
