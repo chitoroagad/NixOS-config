f7982c04168e72aacae6cfbe21222cfdcb88b99a
chitoroagad <darius.chitoroaga@pm.me>
2024-08-18 12:16:34 +0100

    update + sign commits in git

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -4,6 +4,10 @@
     userName = "chitoroagad";
     userEmail = "darius.chitoroaga@pm.me";
     lfs.enable = true;
+    signing = {
+      key = "D3DB98147AF40F5ECCE75E54FEAC7C9776796F2C";
+      signByDefault = true;
+    };
     extraConfig = {
       color.ui = true;
       core.editor = "nvim";
