12b7a44f01b2d9cfc62de52eec65768b8b7abf88
chitoroagad <darius.chitoroaga@pm.me>
2024-09-05 14:13:30 +0100

    removed network from ags config

--- home/desktop/common/ags/widget/bar/buttons/SystemIndicators.ts
+++ home/desktop/common/ags/widget/bar/buttons/SystemIndicators.ts
@@ -90,7 +90,7 @@ export default () =>
 			ProfileIndicator(),
 			DNDIndicator(),
 			BluetoothIndicator(),
-			NetworkIndicator(),
+			//NetworkIndicator(),
 			AudioIndicator(),
 			MicrophoneIndicator(),
 		]),
