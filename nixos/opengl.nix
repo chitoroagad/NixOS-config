ac37b83cd45cc35374cb4318b3cb69ca3216e02a
chitoroagad <darius.chitoroaga@pm.me>
2024-10-26 14:01:42 +0100

    updated opencl extraPackages, flake.lock: update

--- nixos/opengl.nix
+++ nixos/opengl.nix
@@ -4,9 +4,6 @@
     graphics = {
       enable = true;
       extraPackages = with pkgs; [
-        rocm-opencl-icd
-        rocm-opencl-runtime
-        amdvlk # Use AMD Vulkan drivers as needed
         vaapiVdpau
         libvdpau-va-gl
       ];
