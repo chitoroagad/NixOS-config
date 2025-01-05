143265a810d5922a9c927ac647acb9f4b55896f8
chitoroagad <darius.chitoroaga@pm.me>
2025-01-05 13:13:44 +0000

    fixed pulseaudio rename

--- nixos/sound.nix
+++ nixos/sound.nix
@@ -1,12 +1,14 @@
 {...}: {
   # Enable sound
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
+  services = {
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
     pulseaudio.enable = false;
   };
 }
