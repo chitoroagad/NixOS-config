2833d3aca5147c668126b1829f528831908addbf
chitoroagad <darius.chitoroaga@pm.me>
2025-01-06 11:25:34 +0000

    fixed rocm libraries + ollama

--- nixos/ollama.nix
+++ nixos/ollama.nix
@@ -3,7 +3,7 @@
     package = pkgs.ollama;
     enable = true;
     acceleration = "rocm";
-    rocmOverrideGfx = "11.0.1";
+    rocmOverrideGfx = "11.0.2";
     environmentVariables = {
       OLLAMA_KEEP_ALIVE = "30s";
       OLLAMA_LLM_LIBRARY = "rocm";
