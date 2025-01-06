6233409f7a878954f91d1fb2bed8e173dd56bcfb
chitoroagad <darius.chitoroaga@pm.me>
2025-01-06 01:01:44 +0000

    flake.lock: update + tweaks to fidget.nvim

--- home/cli/nvim/lua/plugins/fidget.lua
+++ home/cli/nvim/lua/plugins/fidget.lua
@@ -1,17 +1,20 @@
 return {
 	"j-hui/fidget.nvim",
+	tag = "v1.5.0",
 	lazy = false,
 	priority = 998,
 	opts = {
 		progress = {
 			display = {
+				render_limit = 6,
 				skip_history = false,
+				priority = 0,
 			},
 			ignore = { "ltex" },
 		},
 		notification = {
-			override_vim_notify = true,
 			history_size = 200,
+			override_vim_notify = true,
 			window = {
 				winblend = 0,
 			},
