b5b9a52273d80a813fd6c32a3e683d839d14434a
chitoroagad <darius.chitoroaga@pm.me>
2025-01-08 16:05:11 +0000

    added smart gaps to hyprland

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -88,6 +88,18 @@
         touchpad.natural_scroll = "yes";
         sensitivity = 0.2;
       };
+
+      # smart gaps
+      workspace = [
+        "w[tv1], gapsout:0, gapsin:0"
+        "f[1], gapsout:0, gapsin:0"
+      ];
+      windowrulev2 = [
+        "bordersize 0, floating:0, onworkspace:w[tv1]"
+        "rounding 0, floating:0, onworkspace:w[tv1]"
+        "bordersize 0, floating:0, onworkspace:f[1]"
+        "rounding 0, floating:0, onworkspace:f[1]"
+      ];
       bind = let
         defaultApp = type: "${lib.getExe pkgs.handlr-regex} launch ${type}";
         notify-send = lib.getExe' pkgs.libnotify "nofity-send";
