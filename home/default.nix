7d3e5f9b2367b512623f95bfbda12076537294c6
chitoroagad <darius.chitoroaga@pm.me>
2025-01-09 12:17:46 +0000

    switched to working version of zoom

--- home/default.nix
+++ home/default.nix
@@ -98,10 +98,16 @@
         discord
         vlc
         pavucontrol
-        stable.zoom-us
         obs-studio
         obsidian
         gimp
+        (pkgs.zoom-us.overrideAttrs {
+          version = "6.2.11.5069";
+          src = pkgs.fetchurl {
+            url = "https://zoom.us/client/6.2.11.5069/zoom_x86_64.pkg.tar.xz";
+            hash = "sha256-k8T/lmfgAFxW1nwEyh61lagrlHP5geT2tA7e5j61+qw=";
+          };
+        })
       ]
       ++ [
         inputs.vigiland.packages.${pkgs.system}.vigiland
