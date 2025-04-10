8830f4d3d6c2b2bdab1cfacee0fedaea81cfcca9
chitoroagad <darius.chitoroaga@pm.me>
2025-04-10 17:11:10 +0100

    specified upower settings

--- nixos/upower.nix
+++ nixos/upower.nix
@@ -1,1 +1,9 @@
-{services.upower.enable = true;}
+{
+  services.upower = {
+    enable = true;
+    percentageLow = 20;
+    percentageCritical = 5;
+    percentageAction = 2;
+    criticalPowerAction = "PowerOff";
+  };
+}
