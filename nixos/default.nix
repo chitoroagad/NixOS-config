8bf47b3f013f629832741311b12a21b8c3c0f984
chitoroagad <darius.chitoroaga@pm.me>
2025-05-22 20:31:49 +0100

    flake.lock: update

--- nixos/default.nix
+++ nixos/default.nix
@@ -90,6 +90,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  services.timesyncd.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
