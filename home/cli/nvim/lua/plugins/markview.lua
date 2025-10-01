4a507265e2bfff89bac11017ff0c38efed8fc7f0
chitoroagad <darius.chitoroaga@pm.me>
2025-10-01 17:53:53 +0100

    nvim: made markview look nice with latex blocks

--- home/cli/nvim/lua/plugins/markview.lua
+++ home/cli/nvim/lua/plugins/markview.lua
@@ -1,3 +1,7 @@
+local is_latex = function()
+	return vim.bo.filetype == "tex" or vim.bo.filetype == "latex"
+end
+
 return {
 	"OXY2DEV/markview.nvim",
 	dependencies = {
@@ -8,21 +12,36 @@ return {
 	lazy = false,
 	priority = 49,
 	config = function()
-		local presets = require("markview.presets").headings
 		require("markview").setup({
 			preview = {
 				icons = "devicons",
 				modes = { "i", "n", "no", "c" },
-				hybrid_modes = { "i" },
+				hybrid_modes = { "i", "n" },
 				linewise_hybrid_mode = true,
+				debounce = 1,
 			},
 			latex = {
-				blocks = {
-					pad_amount = 1,
-				},
+				enable = is_latex(),
 			},
 			markdown = {
-				headings = presets.glow,
+				list_items = {
+					shift_width = function(buffer, item)
+						--- Reduces the `indent` by 1 level.
+						---
+						---         indent                      1
+						--- ------------------------- = 1 ÷ --------- = new_indent
+						--- indent * (1 / new_indent)       new_indent
+						---
+						local parent_indnet = math.max(1, item.indent - vim.bo[buffer].shiftwidth)
+
+						return item.indent * (1 / (parent_indnet * 2))
+					end,
+					marker_minus = {
+						add_padding = function(_, item)
+							return item.indent > 1
+						end,
+					},
+				},
 			},
 		})
 	end,
