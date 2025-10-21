19a681b47fefb1b5dbec667227bcd2a444de34fc
chitoroagad <darius.chitoroaga@pm.me>
2025-10-21 23:06:14 +0100

    git: updated to new module shape

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -1,14 +1,14 @@
 {...}: {
   programs.git = {
     enable = true;
-    userName = "chitoroagad";
-    userEmail = "darius.chitoroaga@pm.me";
     lfs.enable = true;
     signing = {
       key = "D3DB98147AF40F5ECCE75E54FEAC7C9776796F2C";
       signByDefault = true;
     };
-    extraConfig = {
+    settings = {
+      user.name = "chitoroagad";
+      user.email = "darius.chitoroaga@pm.me";
       color.ui = true;
       core.editor = "nvim";
       credential.helper = "store";
